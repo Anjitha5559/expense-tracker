4db7c6d5e5b55456df503a99e58845f91ac55bdc
anjitha <anjithajsobha@gmail.com>
2022-06-14 21:26:08 +0530

    Anjitha until operations

--- expense tracker 2/index.js
+++ expense tracker 2/index.js
@@ -4,10 +4,31 @@ function id(id){
 function cls(cls){
     return document.getElementsByClassName(cls);
 }
-function clr(expbtn1){
-    var clr = cls(expinp);
-    for(var i=0;i<clr.length;i++){
-        clr[i].value=";"
-    }
-    
+
+
+
+
+function clr(obj){  
+ classes=cls(obj);
+ console.log(classes);
+ for (var i=0;i<classes.length;i++){
+    classes[i].value="";
+ }
+   
 }
+function showdata(cat1,amt1,box){
+    var cat= id(cat1).value;
+    var amt= id(amt1).value;
+    if (cat==""){
+        alert("plz enter category");
+        return ;
+    }
+   if (amt==""){
+        alert("plz enter amount");
+        return ;
+    }
+    id(box).innerHTML+='<div class="data"><div class="datacat">'+cat+'</div><div class="dataamt">'+amt+'</div></div>';    
+
+
+}
+
